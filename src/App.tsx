0819247041383d935c1f086394db03fa771432bd
Alexandre Jung <jungalexandre@ymail.com>
2022-07-20 00:44:51 +0200

    add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Logo from '@/components/Logo';
 function App() {
   return (
     <>
-      <Logo className="text-light bg-dark" />
+      <Logo />
       <h1 className="fs-medium fw-medium text-gray-4">
         Chez vous, partout et ailleurs
       </h1>
