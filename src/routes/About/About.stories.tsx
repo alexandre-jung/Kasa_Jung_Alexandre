1e5833b04e8bbb737bfd19ea61fe9265c5612363
Alexandre Jung <jungalexandre@ymail.com>
2022-07-23 17:34:10 +0200

    :sparkles: implement about view

---
+++ src/routes/About/About.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import AboutComponent from '@/routes/About';
+import { BrowserRouter } from 'react-router-dom';
+
+export default {
+  title: 'routes/About',
+  component: AboutComponent,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'padded',
+  },
+  decorators: [(Story) => <BrowserRouter>{Story()}</BrowserRouter>],
+} as ComponentMeta<typeof AboutComponent>;
+
+const Template: ComponentStory<typeof AboutComponent> = () => (
+  <AboutComponent />
+);
+
+export const About = Template.bind({});
--- src/routes/About/index.tsx
+++ src/routes/About/index.tsx
@@ -1,5 +1,46 @@
+import Dropdown from '@/components/Dropdown';
+import Hero from '@/components/Hero';
+import styles from './styles.module.scss';
+
 const About = () => {
-  return <div className='fs-large'>A propos</div>;
+  return (
+    <div className={styles.container}>
+      <Hero background="mountain" />
+      <div className={styles['container-compact']}>
+        <Dropdown label="Fiabilité">
+          <p className="fw-light">
+            Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+            photos sont conformes aux logements, et toutes les informations sont
+            régulièrement vérifiées par nos équipes.
+          </p>
+        </Dropdown>
+        <Dropdown label="Respect">
+          <p className="fw-light">
+            La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+            comportement discriminatoire ou de perturbation du voisinage
+            entraînera une exclusion de notre plateforme.
+          </p>
+        </Dropdown>
+        <Dropdown label="Service">
+          <p className="fw-light">
+            Nos équipes se tiennent à votre disposition pour vous fournir une
+            expérience parfaite. N&apos;hésitez pas à nous contacter si vous
+            avez la moindre question.
+          </p>
+        </Dropdown>
+        <Dropdown label="Sécurité">
+          <p className="fw-light">
+            La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+            pour les voyageurs, chaque logement correspond aux critères de
+            sécurité établis par nos services. En laissant une note aussi bien à
+            l&apos;hôte qu&apos;au locataire, cela permet à nos équipes de
+            vérifier que les standards sont bien respectés. Nous organisons
+            également des ateliers sur la sécurité domestique pour nos hôtes.
+          </p>
+        </Dropdown>
+      </div>
+    </div>
+  );
 };
 
 export default About;
